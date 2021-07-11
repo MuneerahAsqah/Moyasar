79a8fc5dae84d8a436b6709eb9bb27de8939663a
مُنيرة عليّ <muneerahalasqah>
2021-07-11 19:26:04 +0300

    front end updated

---
+++ STCpay/Model/PaymentMethod.php
@@ -0,0 +1,8 @@
+<?php
+namespace Moyasar\STCpay\Model;
+
+class PaymentMethod extends \Magento\Payment\Model\Method\AbstractMethod
+{
+    protected $_code = 'stcpayment';
+}
+?>
--- STCpay/view/frontend/web/js/view/payment/method-renderer.js
+++ STCpay/view/frontend/web/js/view/payment/method-renderer.js
@@ -1,17 +1,19 @@
 define(
-    [
-        'uiComponent',
-        'Magento_Checkout/js/model/payment/renderer-list'
-    ],
-    function (Component,
-              rendererList) {
-        'use strict';
-        rendererList.push(
-            {
-                type: 'stcpay',
-                component: 'Moyasar_STCpay/js/view/payment/method-renderer/stcpay-method'
-            }
-        );
-        return Component.extend({});
-    }
+    [
+        'uiComponent',
+        'Magento_Checkout/js/model/payment/renderer-list'
+    ],
+    function (
+        Component,
+        rendererList
+    ) {
+        'use strict';
+        rendererList.push(
+            {
+                type: 'stcpayment',
+                component: 'Moyasar_STCpay/js/view/payment/method-renderer/stcpayment'
+            }
+        );
+        return Component.extend({});
+    }
 );
